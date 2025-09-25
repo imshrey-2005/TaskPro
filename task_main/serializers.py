713dd748a5b9a9537a6baa30e358333bb2f64b09
imshrey-2005 <114224425+imshrey-2005@users.noreply.github.com>
2025-09-25 15:53:06 +0530

    Update serializers.py

--- task_main/serializers.py
+++ task_main/serializers.py
@@ -1,5 +1,7 @@
+from django.forms import ValidationError
 from rest_framework.serializers import ModelSerializer, ChoiceField
 from .models import Tasks, priority_choices,status_choices
+from django.utils import timezone
 
 class TaskListCreateSerializer(ModelSerializer):
     priority = ChoiceField(choices=priority_choices)
@@ -7,7 +9,29 @@ class TaskListCreateSerializer(ModelSerializer):
     
     class Meta:
         model=Tasks
-        fields=['title','description','expiry','priority','status']
+        fields=['title','slug','description','expiry','priority','status']
     
-class TaskFilterByStatusSerializer(ModelSerializer):
-    pass
+class TaskCreateSerializer(ModelSerializer):
+    priority = ChoiceField(choices=priority_choices)
+    status=ChoiceField(choices=status_choices)
+    class Meta:
+        model=Tasks
+        fields=['title','description','expiry','priority','status']
+
+    def validate_title(self, value):
+        task=Tasks.objects.filter(title=value)
+        if Tasks.objects.filter(title=value).exists():
+            if task['expiry']<timezone.now():
+                task.delete()
+            raise ValidationError("Task with this title already exists.")
+        return value
+
+
+class TaskEditSerializer(ModelSerializer):
+    priority = ChoiceField(choices=priority_choices)
+    status=ChoiceField(choices=status_choices)
+    class Meta:
+        model=Tasks
+        fields=['title','slug','description','expiry','priority','status']
+
+   
