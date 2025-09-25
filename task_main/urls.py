1cad85f62e4ed693d5c5c86f657be60c69d59ab1
imshrey-2005 <114224425+imshrey-2005@users.noreply.github.com>
2025-09-25 15:52:24 +0530

    Update urls.py

--- task_main/urls.py
+++ task_main/urls.py
@@ -3,8 +3,10 @@ from . import views
 urlpatterns=[
     path('',views.TaskListView.as_view(), name='tasklist'),
     path('create/',views.TaskCreateView.as_view(), name='taskcreate'),
+    path('search/',views.TaskSearchView.as_view(), name='tasksearch'),
     path('edit/<slug:slug>',views.TaskEditView.as_view(), name='taskedit'),
     path('delete/<slug:slug>',views.TaskDeleteView.as_view(), name='taskdelete'),
+    path('toggle/<slug:slug>',views.TaskToggleView.as_view(), name='tasktoggle'),
     
     
 ]
